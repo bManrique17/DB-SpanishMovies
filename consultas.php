c83c4865a43b19270855c768fbba2db2eeb5c736
bManrique17 <bmanrique17@gmail.com>
2019-05-27 23:07:41 -0600

    Testing something

--- consultas.php
+++ consultas.php
@@ -156,7 +156,9 @@
 		}		
 		echo "</tbody></table><br>";
 	?>
-     
+	<form action="index.php" method="post">
+		<button type="submit" name="btn_volver" class="btn btn-primary">Volver</button>					
+	</form>	
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
